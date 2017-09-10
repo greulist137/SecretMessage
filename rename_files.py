8cf416aa8d0e391308789e767c987b027822c6c2
Steven Greulich <greulich.steven@gmail.com>
2017-09-10 05:53:34 -0700

    final commit
    completion of program that renames the files to not include numbers

--- rename_files.py
+++ rename_files.py
@@ -9,5 +9,9 @@ import os
 def rename_files():
     file_list = os.listdir(r"C:\Users\greul\Downloads\prank\prank")
     print (file_list)
+    os.chdir(r"C:\Users\greul\Downloads\prank\prank")
+    translation_table = dict.fromkeys(map(ord, '0123456789'), None)
+    for file_name in file_list:
+        os.rename(file_name, file_name.translate(translation_table))
     
 rename_files()
